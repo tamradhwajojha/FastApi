187729025da6b9ae1a89a97f5147d4ac8cf7dad6
Tamradhwaj Ojha <tamradhwajojhaji@gmail.com>
2026-09-08 22:25:28 +0530

    basic route of get

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = FastAPI()
 
 def home():
    return {"message": "welcome to the FastAPI application!at venv"}
+
+@app.get("/about")
+def about():
+   return {"message": "this is about page"}  
+
+@app.get("/users")
+
+def users():
+   return {"users": ["rahul","akash","ram","shayam"]}
